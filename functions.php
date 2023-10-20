352277bfb9082d9f0a8b3cb7213e6c8b81bf3bef
Prateek Angel <prateekangel8127@gmail.com>
2023-10-20 06:58:56 -0400

    Adding all files

--- functions.php
+++ functions.php
@@ -6,4 +6,15 @@ wp_enqueue_style('bootstrap',"https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/
  
  wp_enqueue_script('bootstrap','https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js', array('jquery'), null, true );
 });
-?>
+ function setup_support(){
+    add_theme_support('title-tag');
+    add_theme_support('custom-logo',[
+        'header-text' => ['site-title','site-description'],
+        'height' => 100,
+        'width' => 400,
+        'flex-height' => true,
+        'flex-width' => true
+    ]);
+ }
+ add_action('after_setup_theme','setup_support');
+ ?>
